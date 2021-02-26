53511c58d524eeaef3a6144e6cadd05f32195cbf
2Timeowl <78010388+2Timeowl@users.noreply.github.com>
2021-02-26 08:16:27 +0530

    Update T2-fisher.sh

--- T2-fisher.sh
+++ T2-fisher.sh
@@ -516,20 +516,20 @@ site_vk() {
 main_menu() {
 	{ clear; banner; echo; }
 	cat <<- EOF
-		${RED}[${WHITE}::${RED}]${ORANGE} Select An Attack For Your Victim ${RED}[${WHITE}::${RED}]${ORANGE}
+		${RED}[${WHITE}::${RED}]${BLUE} Select An Attack For Your Victim ${RED}[${WHITE}::${RED}]${BLUE}
 
-		${RED}[${WHITE}01${RED}]${ORANGE} Facebook      ${RED}[${WHITE}11${RED}]${ORANGE} Twitch       ${RED}[${WHITE}21${RED}]${ORANGE} DeviantArt
-		${RED}[${WHITE}02${RED}]${ORANGE} Instagram     ${RED}[${WHITE}12${RED}]${ORANGE} Pinterest    ${RED}[${WHITE}22${RED}]${ORANGE} Badoo
-		${RED}[${WHITE}03${RED}]${ORANGE} Google        ${RED}[${WHITE}13${RED}]${ORANGE} Snapchat     ${RED}[${WHITE}23${RED}]${ORANGE} Origin
-		${RED}[${WHITE}04${RED}]${ORANGE} Microsoft     ${RED}[${WHITE}14${RED}]${ORANGE} Linkedin     ${RED}[${WHITE}24${RED}]${ORANGE} DropBox	
-		${RED}[${WHITE}05${RED}]${ORANGE} Netflix       ${RED}[${WHITE}15${RED}]${ORANGE} Ebay         ${RED}[${WHITE}25${RED}]${ORANGE} Yahoo		
-		${RED}[${WHITE}06${RED}]${ORANGE} Paypal        ${RED}[${WHITE}16${RED}]${ORANGE} Quora        ${RED}[${WHITE}26${RED}]${ORANGE} Wordpress
-		${RED}[${WHITE}07${RED}]${ORANGE} Steam         ${RED}[${WHITE}17${RED}]${ORANGE} Protonmail   ${RED}[${WHITE}27${RED}]${ORANGE} Yandex			
-		${RED}[${WHITE}08${RED}]${ORANGE} Twitter       ${RED}[${WHITE}18${RED}]${ORANGE} Spotify      ${RED}[${WHITE}28${RED}]${ORANGE} StackoverFlow
-		${RED}[${WHITE}09${RED}]${ORANGE} Playstation   ${RED}[${WHITE}19${RED}]${ORANGE} Reddit       ${RED}[${WHITE}29${RED}]${ORANGE} Vk
-		${RED}[${WHITE}10${RED}]${ORANGE} Tiktok        ${RED}[${WHITE}20${RED}]${ORANGE} Adobe        ${RED}[${WHITE}30${RED}]${ORANGE} XBOX
+		${RED}[${WHITE}01${RED}]${BLUE} Facebook      ${RED}[${WHITE}11${RED}]${BLUE} Twitch       ${RED}[${WHITE}21${RED}]${BLUE} DeviantArt
+		${RED}[${WHITE}02${RED}]${BLUE} Instagram     ${RED}[${WHITE}12${RED}]${BLUE} Pinterest    ${RED}[${WHITE}22${RED}]${BLUE} Badoo
+		${RED}[${WHITE}03${RED}]${BLUE} Google        ${RED}[${WHITE}13${RED}]${BLUE} Snapchat     ${RED}[${WHITE}23${RED}]${BLUE} Origin
+		${RED}[${WHITE}04${RED}]${BLUE} Microsoft     ${RED}[${WHITE}14${RED}]${BLUE} Linkedin     ${RED}[${WHITE}24${RED}]${BLUE} DropBox	
+		${RED}[${WHITE}05${RED}]${BLUE} Netflix       ${RED}[${WHITE}15${RED}]${BLUE} Ebay         ${RED}[${WHITE}25${RED}]${BLUE} Yahoo		
+		${RED}[${WHITE}06${RED}]${BLUE} Paypal        ${RED}[${WHITE}16${RED}]${BLUE} Quora        ${RED}[${WHITE}26${RED}]${BLUE} Wordpress
+		${RED}[${WHITE}07${RED}]${BLUE} Steam         ${RED}[${WHITE}17${RED}]${BLUE} Protonmail   ${RED}[${WHITE}27${RED}]${BLUE} Yandex			
+		${RED}[${WHITE}08${RED}]${BLUE} Twitter       ${RED}[${WHITE}18${RED}]${BLUE} Spotify      ${RED}[${WHITE}28${RED}]${BLUE} StackoverFlow
+		${RED}[${WHITE}09${RED}]${BLUE} Playstation   ${RED}[${WHITE}19${RED}]${BLUE} Reddit       ${RED}[${WHITE}29${RED}]${BLUE} Vk
+		${RED}[${WHITE}10${RED}]${BLUE} Tiktok        ${RED}[${WHITE}20${RED}]${BLUE} Adobe        ${RED}[${WHITE}30${RED}]${BLUE} XBOX
 
-		${RED}[${WHITE}99${RED}]${ORANGE} About         ${RED}[${WHITE}00${RED}]${ORANGE} Exit
+		${RED}[${WHITE}99${RED}]${BLUE} About         ${RED}[${WHITE}00${RED}]${BLUE} Exit
 
 	EOF
 	
